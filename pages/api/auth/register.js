1025574edf02653db9d2933e5514fb5c9e885022
ryanabi-RA <findlink09@gmail.com>
2022-12-30 14:04:38 +0700

    add fitur: register user

---
+++ pages/api/auth/register.js
@@ -0,0 +1,25 @@
+import db from "../../../libs/db";
+import bcrypt from 'bcryptjs';
+
+export default async function handler(req, res) {
+    if(req.method !== "POST") return res.status(405).end();
+
+    const { email, password } = req.body;
+
+    const salt = bcrypt.genSaltSync(10);
+    const passwordHash = bcrypt.hashSync(password, salt);
+    // console.log(passwordHash);
+
+    const register = await db('users').insert({
+        email,
+        password: passwordHash
+    });
+
+    const registeredUser = await db('users').where({ id: register }).first();
+                                    
+    res.status(200);
+    res.json({
+        massage: "user register successfully",
+        data: registeredUser
+    });
+}
