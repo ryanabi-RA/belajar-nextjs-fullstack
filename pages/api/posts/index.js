baed5cdfce2783523b2bce0a8d8887d8e2c98515
ryanabi-RA <findlink09@gmail.com>
2022-12-30 23:10:05 +0700

    Implementasi authorization middleware ke Post CRUD

--- pages/api/posts/index.js
+++ pages/api/posts/index.js
@@ -1,7 +1,12 @@
 import db from '../../../libs/db';
+import authorization from '../../../middlewares/authorization';
 
 export default async function handler(req, res) {
     if (req.method !== 'GET') return res.status(405).end();
+
+    const auth = await authorization(req, res);
+
+    console.log(auth);
 
     const posts = await db('posts');
 
