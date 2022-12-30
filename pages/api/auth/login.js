da115cad3b46ebd4f8cd6b2196c22415bdd02a54
ryanabi-RA <findlink09@gmail.com>
2022-12-30 16:39:17 +0700

    add fitur: login user

---
+++ pages/api/auth/login.js
@@ -0,0 +1,30 @@
+import db from"../../../libs/db";
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+
+export default async function handler(req, res) {
+    if(req.method !== 'POST') return res.status(405).end();
+
+    const { email, password } = req.body;
+
+    const checkUser = await db('users').where({ email }).first();
+
+    if(!checkUser) return res.status(401).end();
+
+    const checkPassword = await bcrypt.compare(password, checkUser.password);
+
+    if(!checkPassword) return res.status(401).end();
+
+    const token = jwt.sign({
+        id: checkUser.id,
+        email: checkUser.email
+    }, 'tokenSecret', {
+        expiresIn: '7d'
+    });
+
+    res.status(200);
+    res.json({
+        massage: 'Login Successfully',
+        token
+    });
+}
