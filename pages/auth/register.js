b860dd576891da87ef8db7c85b3bfec66ea3f32d
ryanabi-RA <findlink09@gmail.com>
2022-12-31 12:56:34 +0700

    update minor: validation register

--- pages/auth/register.js
+++ pages/auth/register.js
@@ -13,6 +13,8 @@ export default function Register() {
 
         setStatus('loading');
 
+        if (!fields.email || !fields.password) return setStatus('masukkan email & password');
+
         const registerReq = await fetch('/api/auth/register', {
             method: 'POST',
             body: JSON.stringify(fields),
@@ -41,7 +43,7 @@ export default function Register() {
         <div className="w-full h-screen flex justify-center items-center">
             <div className="relative">
                 <div className="w-full h-full bg-blue-900 rounded-2xl blur-sm absolute"></div>
-                <div className="m-1 px-8 pb-10 border-2 border-blue-500 bg-black rounded-xl flex flex-col justify-center items-center relative">
+                <div className="m-[6px] px-8 pb-10 border-2 border-blue-500 bg-black rounded-xl flex flex-col justify-center items-center relative">
                     <h1 className="my-14 text-4xl text-gray-300 font-bold">Register</h1>
                     <form className="flex flex-col space-y-5 items-center" onSubmit={registerHandler.bind(this)}>
                         <div className="text-base text-emerald-500 font-medium">{status}</div>
@@ -50,14 +52,19 @@ export default function Register() {
                             type="text"
                             onChange={fieldHandler.bind(this)}
                             placeholder="Email"
-                            className="p-2 w-64 rounded-lg bg-transparent border-2 border-gray-700" />
+                            className="p-2 w-64 rounded-lg bg-transparent border-2 border-gray-700"
+                            required
+                        />
                         <input
                             name="password"
                             type="password"
                             onChange={fieldHandler.bind(this)}
                             placeholder="Password"
-                            className="p-2 w-64 rounded-lg bg-transparent border-2 border-gray-700" />
-                        <button type="submit" className="m-auto py-1 px-10 bg-blue-800 hover:bg-blue-600 active:bg-transparent rounded-2xl text-lg hover:bg-transparent border-2 border-blue-700 hover:border-blue-500 font-medium">Register</button>
+                            className="p-2 w-64 rounded-lg bg-transparent border-2 border-gray-700"
+                            required
+                        />
+                        <button type="submit"
+                            className="m-auto py-1 px-10 bg-blue-800 hover:bg-transparent active:bg-blue-500 active:rounded-2xl rounded-2xl text-lg  border-2 border-blue-700 hover:border-blue-500 font-medium overflow-hidden">Register</button>
                     </form>
                 </div>
             </div>
