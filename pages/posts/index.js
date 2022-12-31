33e26d04b541ae4bb4337e9fa5a531a953f639e2
ryanabi-RA <findlink09@gmail.com>
2022-12-31 17:34:13 +0700

    add fitur: auth page & read data posts

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,5 +1,36 @@
-export default function index() {
-  return (
-    <div>index</div>
-  )
+import { authPage } from "../../middlewares/authorizationPage"
+
+export async function getServerSideProps(ctx) {
+    const { token } = await authPage(ctx);
+
+    const baseUrl = 'http://localhost:3000';
+
+    const postReq = await fetch(baseUrl + '/api/posts', {
+        headers: {
+            'Authorization': 'Bearer ' + token
+        }
+    });
+
+    const posts = await postReq.json();
+    // console.log(posts);
+
+    return {
+        props: {
+            posts: posts.data
+        }
+    }
+}
+
+
+export default function index(props) {
+    console.log(props);
+    return (
+        <div>
+            <h1>Posts Page</h1>
+
+            {props.posts.map(post => (
+                <div key={post.id}>{post.title}</div>
+            ))}
+        </div>
+    )
 }
